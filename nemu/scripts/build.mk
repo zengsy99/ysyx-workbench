c0f063ae666036820844d2e44ab87cc17eb4ad4a
zengsy99 <zengsiyuan17@gmail.com>
2024-09-05 19:23:28 +0800

    Add a makeing rule of Pre-compilation to .i file

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -32,6 +32,7 @@ $(OBJ_DIR)/%.o: %.c
 	@echo + CC $<
 	@mkdir -p $(dir $@)
 	@$(CC) $(CFLAGS) -c -o $@ $<
+	@$(CC) $(CFLAGS) -E -MF /dev/null $< | clang-format > $(@:.o=.i)
 	$(call call_fixdep, $(@:.o=.d), $@)
 
 $(OBJ_DIR)/%.o: %.cc
